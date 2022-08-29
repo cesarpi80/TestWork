7951676a0ab2aebf6c62aae5538d13fd24c78e2f
Cesar Saenz <cesarsaenztorrez@gmail.com>
2022-08-29 16:57:56 -0600

    Agregar archivos de proyecto.

---
+++ TestWork/Program.cs
@@ -0,0 +1,32 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+var listaFibo = new[]
+{
+    0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765, 10946, 17711, 28657, 46368
+};
+
+app.MapGet("/indexFibo", (int index) =>
+{
+    var num = listaFibo[index];
+    return num;
+})
+.WithName("GetListaFibo");
+
+app.Run();
+
